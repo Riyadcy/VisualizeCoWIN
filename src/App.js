0c8533c14f36c1670f125a3202f0b454e096a3e4
Faraaz Biyabani <faraazb@protonmail.com>
2021-08-16 12:56:10 +0530

    Create Test Line chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+
+const data = {
+  labels: ['Date 1', 'Date 2', 'Date 3', 'Date 4', 'Date 5', 'Date 6'],
+  datasets: [
+    {
+      label: 'Dose 1',
+      data: [100, 200, 100, 150, 200, 250],
+      fill: false,
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgba(255, 99, 132, 0.2)',
+    },
+    {
+      label: 'Dose 2',
+      data: [80, 60, 200, 55, 200, 80],
+      fill: false,
+      backgroundColor: 'rgb(54, 162, 235)',
+      borderColor: 'rgba(54, 162, 235, 0.2)',
+    },
+  ],
+};
+
+const options = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+};
+
+const LineChart = () => (
+    <>
+      <div className='header'>
+        <h1 className='title'>Test Chart</h1>
+      </div>
+      <Line data={data} options={options} />
+    </>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="chart-container">
+        <div className="chart">
+          <LineChart />
+        </div>
+      </div>
     </div>
   );
 }
